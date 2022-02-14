4c5348815cd87f92735ea8934b5484fc2105fe89
maxuelong <maxuelong@baidu.com>
2022-02-14 17:06:54 +0800

    fix: change directory

--- packages/vue/components/index.ts
+++ packages/vue/components/index.ts
@@ -1,8 +1,8 @@
-import Button from "./button.vue";
+import Button from "./button/button.vue";
 export { Button };
 
 
-import Icon from "./icon.vue";
+import Icon from "./icon/icon.vue";
 export { Icon };
 
 import Input from "./input/input.vue";
