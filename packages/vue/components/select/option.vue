eb75d8e0ccc42ea38ec8b70ac849a766525e4bf3
下雪 <dl880509@163.com>
2022-03-01 17:17:52 +0800

    merge (#19)
    * fix:  modify input for typecheck
    
    * feat: add select
    
    * fix: select types
    
    * fix: updata eslint
    
    Co-authored-by: maxuelong <maxuelong@baidu.com>

--- packages/vue/components/select/option.vue
+++ packages/vue/components/select/option.vue
@@ -1,38 +1,38 @@
 <script setup lang="ts">
-import { inject, ref, computed } from 'vue'
+import { inject, ref, computed } from 'vue';
 import _styles from 'cosmic-design/select-option.module.css';
 
 const props = defineProps({
     style: {
         type: Object,
-        default: _styles
+        default: _styles,
     },
     label: {
         type: String,
-        required: true
+        required: true,
     },
     value: {
         type: String,
-        required: true
+        required: true,
     },
     styles: {
         type: Object,
-        default: _styles
+        default: _styles,
     },
     disabled: {
         type: Boolean,
-        default: false
-    }
+        default: false,
+    },
 });
 const select = inject('select');
 const size = inject('size');
-const state = ref(props.disabled? "disabled": "normal");
+const state = ref(props.disabled? 'disabled': 'normal');
 
 const changeHandler = () => {
-    select.setSelection({value: props.value, label: props.label})
-}
+    select.setSelection({value: props.value, label: props.label});
+};
 
-const isSelected = computed(() => select.value === props.value)
+const isSelected = computed(() => select.value === props.value);
 
 </script>
 
