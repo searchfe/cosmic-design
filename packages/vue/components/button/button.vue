006e4bab78661cf016ece75a2590db8c5d7d66c1
xingxiaodong01 <xingxiaodong01@baidu.com>
2022-03-04 14:21:36 +0800

    chore: add githook && format vue button

--- packages/vue/components/button/button.vue
+++ packages/vue/components/button/button.vue
@@ -1,28 +1,33 @@
 <script setup lang="ts">
-    import _styles from 'cosmic-design/button.module.css';
-    import { State, Size } from 'cosmic-design';
-    import { ref } from 'vue';
+import _styles from 'cosmic-design/button.module.css';
+import { ref } from 'vue';
     
-    const props = defineProps({
-        styles: {
-            type: Object,
-            default: _styles,
-        },
-        size: {
-            type: String,
-            default: 'xs'
-        },
-        disabled: {
-            type: Boolean,
-            default: false
-        }
-    });
-    const state = ref(props.disabled? "disabled": "normal");
+const props = defineProps({
+    styles: {
+        type: Object,
+        default: _styles,
+    },
+    size: {
+        type: String,
+        default: 'xs',
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+});
+const state = ref(props.disabled? 'disabled': 'normal');
 </script>
 <template>
-<div :class="[styles.button, state, size]">
-<slot name='prefix' :class="[styles.prefix, size]"></slot>
-<slot :class="[styles.label]"></slot>
-<slot name='subfix' :class="[styles.subfix , size]"></slot>
-</div>
+    <div :class="[styles.button, state, size]">
+        <slot
+            name="prefix"
+            :class="[styles.prefix, size]"
+        />
+        <slot :class="[styles.label]" />
+        <slot
+            name="subfix"
+            :class="[styles.subfix , size]"
+        />
+    </div>
 </template>
