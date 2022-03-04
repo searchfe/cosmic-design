e1769708207bc942b6aa163232ec9f2055201f0c
木杉 <zhmushan@qq.com>
2022-03-04 17:47:36 +0800

    feat: grid

---
+++ packages/vue/components/grid/row.ts
@@ -0,0 +1,82 @@
+import { defineComponent, h, PropType, type StyleValue } from "vue";
+
+type Align =
+    | 'start'
+    | 'end'
+    | 'center'
+    | 'baseline'
+
+type Justify =
+    | 'start'
+    | 'end'
+    | 'center'
+    | 'space-around'
+    | 'space-between'
+
+type Row = {
+    spans?: number
+    gutter?: number
+    align?: Align
+    justify?: Justify
+    wrap?: boolean
+    xs?: Row
+    sm?: Row
+    md?: Row
+    lg?: Row
+    xl?: Row
+};
+
+const props = {
+    spans: {
+        type: Number,
+        default: 12
+    },
+    gutter: {
+        type: Number,
+        default: 0
+    },
+    justify: {
+        type: String as PropType<Justify>,
+        default: 'start'
+    },
+    align: {
+        type: String as PropType<Align>,
+        default: 'start'
+    },
+    wrap: {
+        type: Boolean,
+        default: false
+    }
+} as const
+
+export default defineComponent({
+    name: 'Row',
+    props,
+
+    setup(props, { slots }) {
+        const { spans, justify, align, wrap, gutter } = props;
+        return () => h(
+            'section',
+            {
+                style: {
+                    display: 'flex',
+                    justifyContent: ['start', 'end'].includes(justify)
+                        ? 'flex-' + justify
+                        : justify,
+                    alignItems: align,
+                    flexWrap: wrap ? 'wrap' : 'nowrap',
+                    marginLeft: `-${gutter / 2}px`,
+                    marginRight: `-${gutter / 2}px`
+                } as StyleValue
+            },
+            slots.default?.()?.map(el => h(el, {
+                style: {
+                    width: `calc(100% / ${spans / (el.props?.span ?? spans)})`,
+                    paddingLeft: `${gutter / 2}px`,
+                    paddingRight: `${gutter / 2}px`
+                } as StyleValue
+            })
+            )
+        )
+    }
+});
--- packages/vue/components/index.ts
+++ packages/vue/components/index.ts
@@ -14,3 +14,4 @@ export * from './select';
 
 export * from "./menu";
 
+export * from "./grid";
