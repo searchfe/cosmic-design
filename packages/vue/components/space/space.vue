fb9c4158f0614fbafeba8003b778974e003a7ee5
木杉 <zhmushan@qq.com>
2022-03-08 12:12:59 +0800

    feat(vue/space): support gap

--- packages/vue/components/space/space.vue
+++ packages/vue/components/space/space.vue
@@ -1,56 +1,71 @@
 <script setup lang="ts">
+import { computed, ref } from "vue";
+import _styles from 'cosmic-design/space.module.css';
+
 type Align =
-  | 'start'
-  | 'end'
-  | 'center'
-  | 'baseline'
+    | 'start'
+    | 'end'
+    | 'center'
+    | 'baseline'
 
 type Justify =
-  | 'start'
-  | 'end'
-  | 'center'
-  | 'space-around'
-  | 'space-between'
+    | 'start'
+    | 'end'
+    | 'center'
+    | 'space-around'
+    | 'space-between'
 
 type Direction =
-  | 'horizontal'
-  | 'vertical'
+    | 'horizontal'
+    | 'vertical'
 
 type Size =
-  | 'sm'
-  | 'md'
-  | 'lg'
-  | number
+    | 'sm'
+    | 'md'
+    | 'lg'
 
-withDefaults(defineProps<{
-  direction?: Direction,
-  size?: Size | [Size, Size]
-  align?: Align,
-  justify?: Justify,
-  inline?: boolean,
-  wrap?: boolean,
+const props = withDefaults(defineProps<{
+    styles?: CSSModuleClasses
+    direction?: Direction,
+    size?: Size | [Size, Size]
+    align?: Align,
+    justify?: Justify,
+    inline?: boolean,
+    wrap?: boolean,
 }>(), {
-  direction: 'horizontal',
-  size: 'sm',
-  align: 'center',
-  justify: 'start',
-  inline: true,
-  wrap: false
+    styles: _styles,
+    direction: 'horizontal',
+    size: 'sm',
+    align: 'center',
+    justify: 'start',
+    inline: true,
+    wrap: false
 });
 
+const hGap = ref<Size>();
+const vGap = ref<Size>();
+const vertical = computed(() => props.direction === 'vertical');
+
+if (props.size instanceof Array) {
+    hGap.value = props.size[0];
+    vGap.value = props.size[1];
+} else if (props.size) {
+    hGap.value = vGap.value = props.size;
+}
 </script>
 <template>
-  <div
-    :style="{
-      display: inline ? 'inline-flex' : 'flex',
-      flexDirection: direction === 'vertical' ? 'column' : 'row',
-      justifyContent: ['start', 'end'].includes(justify)
-        ? 'flex-' + justify
-        : justify,
-      flexWrap: wrap ? 'wrap' : 'nowrap',
-      alignItems: align
-    }"
-  >
-    <slot />
-  </div>
+    <section
+        :class="[styles.root, direction, vertical ? hGap : vGap]"
+        :style="{
+            display: inline ? 'inline-flex' : 'flex',
+            flexDirection: direction === 'vertical' ? 'column' : 'row',
+            justifyContent: ['start', 'end'].includes(justify)
+                ? 'flex-' + justify
+                : justify,
+            flexWrap: wrap ? 'wrap' : 'nowrap',
+            alignItems: align
+        }"
+    >
+        <slot :class="[vertical ? vGap : hGap]"></slot>
+    </section>
 </template>
