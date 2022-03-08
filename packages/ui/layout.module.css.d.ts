9f75ae78bc165f2aa251098bc3e996a1e3c1e170
qiansc <van3008@163.com>
2022-03-08 18:37:17 +0800

    fix: 🐛 util build

---
+++ packages/ui/layout.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly 'layout': string;
+  readonly 'content': string;
+  readonly 'header': string;
+  readonly 'has-sider': string;
+  readonly 'sider': string;
+};
+export = styles;
+
