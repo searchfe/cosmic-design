ab8feccdef2a8017b9794a6994fa941936ee2e55
maxuelong <maxuelong@baidu.com>
2022-03-15 11:42:46 +0800

    fix: 🐛 update radio for new UI

--- packages/ui/radio/radio-button.module.css.d.ts
+++ packages/ui/radio/radio-button.module.css.d.ts
@@ -1,5 +1,4 @@
 declare const styles: {
-  readonly "root": string;
+    readonly 'radio-button': string;
 };
 export = styles;
-
--- packages/vue/components/radio/radio-group.ts
+++ packages/vue/components/radio/radio-group.ts
@@ -1,7 +1,5 @@
 import { reactive, h, defineComponent } from 'vue';
-import { radioGroup as _styles} from 'cosmic-ui';
 import { type SelectOption, Select } from 'cosmic-common';
-const  styles = _styles;
 
 const props = {
     value: String,
@@ -14,30 +12,31 @@ const props = {
 export default defineComponent({
     name: 'RadioGroup',
     props,
-    setup(props, {slots, emit}) {
-        const selectData = slots.default?.()?.map(el => ({value: el.props.value, label: el.props.label}));
+    setup(props, { slots, emit }) {
+        const selectData = slots.default?.()?.map(el => ({ value: el.props.value, label: el.props.label }));
         const select = reactive(new Select());
         select.setSelectList(selectData);
         select.setSelection(props.value as string);
-        const clickHandler = (data) => {
+        const clickHandler = data => {
             select.setSelection(data);
             emit('onChange', data);
         };
 
-        return () => h(
-            'div',
-            {
-                class: [styles.root, '12'],
-            },
-            slots.default?.()?.map(el => {
-                const {value, label} = el.props as SelectOption;
-                return h(el, {
-                    ...el.props,
-                    size: props.size,
-                    onClick: () => clickHandler({value, label}),
-                    selected: select.selected({value, label}),
-                });
-            }),
-        );
+        return () =>
+            h(
+                'div',
+                {
+                    class: ['flex'],
+                },
+                slots.default?.()?.map(el => {
+                    const { value, label } = el.props as SelectOption;
+                    return h(el, {
+                        ...el.props,
+                        size: props.size,
+                        onClick: () => clickHandler({ value, label }),
+                        selected: select.selected({ value, label }),
+                    });
+                }),
+            );
     },
 });
