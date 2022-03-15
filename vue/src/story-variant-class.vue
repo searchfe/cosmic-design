4151398332fcd52668a292b9fd68ab9965d21212
qiansc <qiansc@users.noreply.github.com>
2022-03-15 04:41:07 +0000

    deploy: 7bb9105a55ca3b074eb3405e75ccdb4927822339

---
+++ vue/src/story-variant-class.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+defineProps({
+    type: String,
+});
+</script>
+<template>
+    <div class="flex flex-col items-center">
+        <div class="flex flex-row items-end">
+            <i-cosmic-circles :class="['w-40 h-40 mr-10', type]" style="border-radius: 6px;" />
+            <i-cosmic-circles :class="['w-32 h-32 disabled', type]" style="border-radius: 6px;" />
+        </div>
+        <span>{{ type }}</span>
+    </div>
+</template>
---
+++ vue/src/story-variant-classes.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import utility from './story-variant-class.vue';
+const colors = ['-v-color-dark', '-v-color-primary', '-v-color-dark-primary'];
+const backgrounds = ['-v-bg-primary', '-v-bg-inapparent', '-v-bg-heavy'];
+const borders = ['-v-border-none', '-v-border-inapparent', '-v-border-apparent', '-v-border-dark'];
+</script>
+<template>
+    <div class="font-bold text-lg mt-16">
+        Variant Classes
+    </div>
+    <div class="font-bold mt-10">
+        Color
+        <span class="text-sm opacity-30">default / disbaled</span>
+    </div>
+    <div class="flex flex-row">
+        <div v-for="item in colors" :key="item">
+            <utility :type="item" class="m-10" />
+        </div>
+    </div>
+    <div class="font-bold mt-10">
+        Background Color
+        <span class="text-sm opacity-30">default / disbaled</span>
+    </div>
+    <div class="flex flex-row">
+        <div v-for="item in backgrounds" :key="item">
+            <utility :type="item" class="m-10" />
+        </div>
+    </div>
+    <div class="font-bold mt-10">
+        Border Color
+        <span class="text-sm opacity-30">default / disbaled</span>
+    </div>
+    <div class="flex flex-row">
+        <div v-for="item in borders" :key="item">
+            <utility :type="item" class="m-10" />
+        </div>
+    </div>
+</template>
