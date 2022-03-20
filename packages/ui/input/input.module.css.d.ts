881783d2446e64bb230aef9eb9a176b33f4c7a24
maxuelong <maxuelong@baidu.com>
2022-03-20 15:25:57 +0800

    fix: 🐛 fix input css

--- packages/ui/input/input.module.css.d.ts
+++ packages/ui/input/input.module.css.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly "root": string;
+  readonly "active": string;
   readonly "input": string;
   readonly "prefix": string;
   readonly "subfix": string;
--- packages/ui/select/select.module.css.d.ts
+++ packages/ui/select/select.module.css.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly "select": string;
+  readonly "active": string;
   readonly "inherit": string;
   readonly "border": string;
   readonly "popover": string;
