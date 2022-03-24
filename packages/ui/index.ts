07de759ecfe4aa68056b50a92b2d60127f2a5dbb
yangguang <47093380+wasaisai@users.noreply.github.com>
2022-03-24 17:10:26 +0800

    feat: add divider by  wasaisai (#98)
    feat: add divider by  wasaisai

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -48,3 +48,5 @@ export { default as popover } from './popover/popover.module.css';
 
 export { default as tabs } from './tabs/tabs.module.css';
 export { default as link } from './link/link.module.css';
+
+export { default as divider} from './divider/divider.module.css';
