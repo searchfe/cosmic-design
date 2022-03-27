c891c7dc12cf94ce57399876c3769ae523db725d
qiansc <van3008@163.com>
2022-03-28 00:27:25 +0800

    fix: 🐛 type for tree

---
+++ .changeset/mean-trainers-kick.md
@@ -0,0 +1,5 @@
+---
+'cosmic-vue': patch
+---
+
+fix type of tree
--- packages/vue/components/tree/index.ts
+++ packages/vue/components/tree/index.ts
@@ -1,4 +1,4 @@
 export { default as Tree } from './tree.vue';
 export { default as TreeNode } from './tree-node.vue';
-export type { TreeProps, TreeNodeProps, TreeNodeEvent, TreeChangeEvent } from './types';
+export type { TreeProps, TreeNodeProps, TreeNodeEvent, TreeChangeEvent, TreeDataProps } from './types';
 export { TreeNodeState } from './types';
