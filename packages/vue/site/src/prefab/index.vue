40fe13e7f2a9d19a21bcdfee80bc16fb236bdd66
wuyan14 <wuyan14@baidu.com>
2022-03-29 18:17:31 +0800

    fix: 🐛 menu click width update
    menu click width update

---
+++ packages/vue/site/src/prefab/index.vue
@@ -0,0 +1,169 @@
+<script lang="ts" setup>
+import { Row, Col, Popover, Card, Input } from 'cosmic-vue';
+import { ref } from 'vue';
+import Yuzhi from './prefab.vue';
+import Cascader from '../../../components/cascader/cascader.vue';
+import styles from './prefab.module.css';
+
+const yuzhiList = {
+    localYz: [{
+        state: 'yz-new',
+        title: '预置1',
+    },
+    {
+        state: 'yz-repeat',
+        title: '预置2',
+    },
+    {
+        state: 'yz-regular',
+        title: '预置3',
+    },
+    ],
+    regularYz:[{
+        state: 'yz-new',
+        title: '预置1',
+    },
+    {
+        state: 'yz-repeat',
+        title: '预置2',
+    },
+    {
+        state: 'yz-regular',
+        title: '预置3',
+    },
+    ],
+};
+const selected = ref([]);
+const source = ref([
+    {
+        name: '视觉搜索',
+        children: [
+            {
+                name: '识货',
+                children: [{ name: '通用版' }, { name: '春节' }, { name: '购物节' }],
+            },
+            {
+                name: '人脸识别',
+            },
+        ],
+    },
+    
+    {
+        name: '视频多模沉浸',
+    },
+    {
+        name: '语音搜索',
+    },
+]);
+function fn() {
+    // eslint-disable-next-line no-console
+    console.log('app....', selected.value);
+}
+const value = ref('');
+</script>
+<template>
+    <div>
+        <div :class="[styles['yz-search']]">
+            <Input
+                v-model:value="value"
+                size="sm"
+                placeholder="搜索"
+            >
+                <template #prefix>
+                    <i-cosmic-caution :class="[styles['yz-icon']]" />
+                </template>
+            </Input>
+        </div>
+        <div :class="[styles['yz-module']]">
+            <div :class="[styles['yz-title']]" class="font-bold text-sm mt-16">
+                <span :class="[styles['yz-title-icon']]"><i-cosmic-arrow-down /></span>
+                <span>文档使用预制</span>
+            </div>
+                
+            <div :class="[styles['yz-container']]">
+                <yuzhi v-for="(item, i) in yuzhiList.localYz" :key="i" :yzdata="item" :class="[styles['yz-content']]">
+                    <template #iconLeft="props">
+                        <i-cosmic-comp :class="props.class" />
+                    </template>
+                    <template #iconRight="props">
+                        <i-cosmic-more :class="props.class" />
+                    </template>
+                </yuzhi>
+            </div>
+            <div>
+                <div :class="[styles['yz-title']]" class="font-bold text-sm mt-16">
+                    <span :class="[styles['yz-title-icon']]"><i-cosmic-arrow-down /></span>
+                    <span>业务规范预制</span>
+                </div>
+                <Row :class="[styles['yz-bus-filter']]">
+                    <Col>
+                        <Cascader
+                            v-model:source="source"
+                            v-model:selected="selected"
+                            popover-height="200px"
+                            @update:selected="fn"
+                        />
+                    </Col>
+                    <Col :class="[styles['yz-filter-container']]">
+                        <Popover placement="bottomRight">
+                            <div :class="[styles['yz-filter']]">
+                                <i-cosmic-more />
+                            </div>
+                            <template #content>
+                                <Card title="筛选组件" :class="[styles['yz-filter-content']]">
+                                    <div :class="[styles['yz-filter-list']]">
+                                        <div :class="[styles['yz-filter-smart']]">
+                                            <p>智能组件</p>
+                                            <div :class="[styles['yz-filter-components']]">
+                                                <span :class="[styles['yz-filter-tag']]">输入框</span>
+                                                <span :class="[styles['yz-filter-tag']]">输入框</span>
+                                                <span :class="[styles['yz-filter-tag']]">输入框</span>
+                                                <span :class="[styles['yz-filter-tag']]">输入框</span>
+                                                <span :class="[styles['yz-filter-tag']]">输入框</span>
+                                                <span :class="[styles['yz-filter-tag']]">输入框</span>
+                                                <span :class="[styles['yz-filter-tag']]">输入框</span>
+                                                <span :class="[styles['yz-filter-tag']]">输入框</span>
+                                                <span :class="[styles['yz-filter-tag']]">输入框</span>
+                                            </div>
+                                        </div>
+                                        <div :class="[styles['yz-filter-type']]">
+                                            <p>类型</p>
+                                            <div :class="[styles['yz-filter-components']]">
+                                                <span :class="[styles['yz-filter-tag']]">基础</span>
+                                                <span :class="[styles['yz-filter-tag']]">复合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                                <span :class="[styles['yz-filter-tag']]">组合</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div :class="[styles['yz-filter-btn']]">
+                                        <span :class="[styles['yz-filter-tag']]">重置</span>
+                                        <span :class="[styles['yz-filter-tag'],styles['yz-filter-close']]">收起</span>
+                                    </div>
+                                </Card>
+                            </template>
+                        </Popover>
+                    </Col>
+                </Row>
+            
+                <div :class="[styles['yz-container']]">
+                    <yuzhi v-for="(item, i) in yuzhiList.regularYz" :key="i" :yzdata="item" :class="[styles['yz-content']]">
+                        <template #iconRight="props">
+                            <i-cosmic-more :class="props.class" />
+                        </template>
+                    </yuzhi>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ packages/vue/site/src/prefab/prefab.vue
@@ -0,0 +1,117 @@
+<template>
+    <div 
+        :class="[styles['yz-card-container'], yzClick]" 
+        @mouseenter="onContententer"
+        @mouseleave="onContentLeave"
+        @click="onContentClick"
+    >
+        <div :class="[styles['yz-card']]">
+            <div :class="[hovering && props.yzdata.state !== 'yz-regular' ? hovering: '']">
+                <Popover placement="bottom">
+                    <section v-if="slots.iconLeft" :class="[styles['header-icon'], props.yzdata.state, size]">
+                        <slot name="iconLeft" />
+                    </section>
+                    <template #content>
+                        <div :class="[styles['yz-tootip']]" class="ope-regular">加入规范</div>
+                    </template>
+                </Popover>
+            </div>
+            <div :class="[hovering]" class="menu">
+                <Menu
+                    :size="state"
+                    value="2"
+                >
+                    <template #activator>
+                        <section v-if="slots.iconRight" :class="[styles['header-icon'], size]">
+                            <slot name="iconRight" />
+                        </section>
+                    </template>
+                    
+                    
+                    <template #menu>
+                        <MenuOption 
+                            v-for="data of opeSelect"
+                            :key="data.value"
+                            size="xl" :value="data.value" :label="data.label" :has-check="false" @on-change="menuChangeHandler"
+                        >
+                            <template #suffix>
+                                7
+                            </template>
+                        </MenuOption>
+                    </template>
+                </Menu>
+            </div>
+        </div>
+        <div v-show="hovering" :class="[styles['yz-tootip'], styles['yz-card-title']]">{{ props.yzdata.title }}</div>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref, useSlots } from 'vue';
+import { Popover } from 'cosmic-vue';
+import styles from './prefab.module.css';
+import Menu from '../../../components/menu/menu.vue';
+import MenuOption from '../../../components/menu/option.vue';
+
+interface Yzdata {
+    state: string;
+    title: string;
+} 
+const props = withDefaults(defineProps<{
+    yzdata: Yzdata
+    // styles?: CSSModuleClasses
+    size?: string;
+}>(), {
+    // styles,
+    size: 'xs',
+});
+const state = ref('sm');
+const slots = useSlots();
+
+const hovering = ref('');
+const yzClick = ref('');
+
+const onContententer = function () {
+    hovering.value = 'yz-hovering';
+};
+const onContentLeave = function () {
+    hovering.value = '';
+};
+const onContentClick = function () {
+    yzClick.value = 'yz-click';
+};
+const menuChangeHandler = () => {
+    // eslint-disable-next-line no-console
+    console.log(11);
+};
+const opeSelect = ref([{
+    label: '高亮显示     7',
+    value: 1,
+},{
+    label: '重命名',
+    value: 2,
+},{
+    label: '编辑预制',
+    value: 3,
+},{
+    label: '重新连接规范',
+    value: 4,
+},{
+    label: '编辑规范',
+    value: 5,
+},{
+    label: '删除规范',
+    value: 6,
+},{
+    label: '脱离规范',
+    value: 7,
+},
+]);
+</script>
+<style scoped>
+    
+    .ope-regular {
+        width: 60px;
+        padding: 2px;
+    }
+</style>
+
