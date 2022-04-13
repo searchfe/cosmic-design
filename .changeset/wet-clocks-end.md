376f1d9658e50a487be816300912182ee5738edf
wuyan14 <wuyan14@baidu.com>
2022-04-13 15:54:14 +0800

    feat: add loading ui

---
+++ .changeset/wet-clocks-end.md
@@ -0,0 +1,5 @@
+---
+'cosmic-ui-alpha': patch
+---
+
+增加 loading 组件
---
+++ packages/ui/loading/loading.module.css.d.ts
@@ -0,0 +1,13 @@
+declare const styles: {
+  readonly "spin": string;
+  readonly "spin-spinning": string;
+  readonly "spin-nested-loading": string;
+  readonly "spin-dot": string;
+  readonly "spin-text": string;
+  readonly "spin-show-text": string;
+  readonly "spin-container": string;
+  readonly "spin-blur": string;
+  readonly "spin-tip": string;
+};
+export = styles;
+
