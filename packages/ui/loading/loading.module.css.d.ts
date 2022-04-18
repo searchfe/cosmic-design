f2bfd2cdfad03ad08ef67c3107775a79f25bfe18
wuyan14 <wuyan14@baidu.com>
2022-04-18 11:28:19 +0800

    feat: add loading ui

--- packages/ui/loading/loading.module.css.d.ts
+++ packages/ui/loading/loading.module.css.d.ts
@@ -2,12 +2,9 @@ declare const styles: {
   readonly "spin": string;
   readonly "spin-spinning": string;
   readonly "spin-nested-loading": string;
-  readonly "spin-dot": string;
-  readonly "spin-text": string;
-  readonly "spin-show-text": string;
+  readonly "spin-nested-spinning": string;
   readonly "spin-container": string;
   readonly "spin-blur": string;
-  readonly "spin-tip": string;
 };
 export = styles;
 
